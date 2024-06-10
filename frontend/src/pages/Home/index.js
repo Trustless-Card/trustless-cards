c1cc7796bb36c17b3728c744949ac48bfd475efe
noymaxx <hugo.povoleri@sou.inteli.edu.br>
2024-06-10 18:13:44 -0300

    config: shadcn.ui init

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { Button } from "../../components/ui/button";
+
 
 export default function Home() {
-	return <h1 className="bg-red-500">oioioi</h1>
+	
+	return <Button variant="outline">Button</Button>;
+
 }
 

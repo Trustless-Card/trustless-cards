c62052546a4fc6256f4fffec7a095156df2e88d5
joao sotto <sottojao@gmail.com>
2024-06-12 14:59:48 -0300

    fix(logo-footer): redirect to the top page

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -7,7 +7,19 @@ export default function Footer() {
   return (
     <footer className="bg-gradient-to-t from-indigo-950 to-black text-white">
       <div className="pt-10 flex justify-center mb-5">
-        <img src={logo} alt="logo" className="h-20 w-20" />
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault(); // Prevent the default link behavior
+            window.scrollTo({ top: 0, behavior: "smooth" }); // Smoothly scroll to the top of the page
+          }}
+        >
+          <img
+            src={logo}
+            alt="logo"
+            className="h-20 w-20 hover:cursor-pointer"
+          />
+        </a>
       </div>
       <div className="text-center text-lg pb-4 w-full">
         <p>Made At Inteli</p>
